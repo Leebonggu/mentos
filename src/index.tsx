8308939281a85ba34c3b43b41891318694b62399
Leebonggu <leebonggu12@hanmail.net>
2022-01-17 22:30:58 +0900

    fix/elsint_rules

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
