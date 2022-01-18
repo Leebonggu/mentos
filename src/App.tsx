c7e3e84552d311327848dc49693d576c60d9f53a
Leebonggu <leebonggu12@hanmail.net>
2022-01-19 01:18:04 +0900

    MT-12_set_router_basename(임시)

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import Header from '@components/Header';
 import Layout from '@components/Layout';
 
 function App() {
+  const isDev = process.env.NODE_ENV === 'development';
+  
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={isDev ? '/' : process.env.PUBLIC_URL}>
       <Header />
       <Layout>
         <RootRouter />
