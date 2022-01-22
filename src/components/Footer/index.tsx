30c823db416e8d4641c9f3a1d4c3d30ae52a9fd2
Leebonggu <leebonggu12@hanmail.net>
2022-01-23 00:10:52 +0900

    MT-13 footer 추가

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,9 +5,14 @@ import {
   Contents,
   LogoWrppaer,
   CorporateInformationWrppaer,
-  SNSWrapper
+  SNSWrapper,
+  Address,
+  CorporateInformationBottom,
+  Copyright,
+  Policy,
 }
 from './styles';
+
 
 function Footer() {
   return (
@@ -16,11 +21,20 @@ function Footer() {
         <LogoWrppaer>
           <Logo isSticky/>
         </LogoWrppaer>
-        <CorporateInformationWrppaer>Info</CorporateInformationWrppaer>
+        <CorporateInformationWrppaer>
+          <Address>대표: 내이름. 서울특별시 중구 한강대로 416, 서울스퀘어 15층 101호</Address>
+          <CorporateInformationBottom>
+            <Copyright>Copyright by (주)Mentos. All right reserved.</Copyright>
+            <Policy>
+              <span>이용약관</span>
+              <span>개인정보처리방침</span>
+            </Policy>
+          </CorporateInformationBottom>
+        </CorporateInformationWrppaer>
         <SNSWrapper>
-          <Icon />
-          <Icon />
-          <Icon />
+          <Icon bgColor='gray_04'/>
+          <Icon bgColor='gray_04' ml={5} />
+          <Icon bgColor='gray_04' ml={5} />
         </SNSWrapper>
       </Contents>
     </Container>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
+  width: 100%;
   height: 160px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  margin-bottom: 0px;
   background-color: ${props => props.theme.color.gray_05};
 `;
 
 export const Contents = styled.div`
   margin: 0 auto;
   max-width: 960px;
-  padding: 32px 0 72px 0;
+  padding: 32px 0 36px 0;
   display: flex;
 `;
 
@@ -16,8 +21,28 @@ export const LogoWrppaer = styled.div`
   flex: 2;
 `
 export const CorporateInformationWrppaer = styled.div`
+  height: 56px;
   flex: 6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: ${props => props.theme.typography.size.xs};
 `;
+
+export const Address = styled.div``;
+export const CorporateInformationBottom = styled.div`
+  line-height: ${props => props.theme.typography.lineHight.xxs};
+`;
+export const Copyright = styled.div``;
+export const Policy = styled.div`
+  span {
+    margin-right: 5px;
+  }
+`;
+
 export const SNSWrapper = styled.div`
   flex: 2;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
 `;
