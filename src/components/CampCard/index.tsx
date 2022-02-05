18311f75ff9af785260cbcce5936013adb45a8a2
Leebonggu <leebonggu12@hanmail.net>
2022-02-05 15:57:13 +0900

    MT-26 dayjs 추가

--- src/components/CampCard/index.tsx
+++ src/components/CampCard/index.tsx
@@ -1,17 +1,18 @@
+import dayjs from 'dayjs';
 import { Camp } from '@/typings/index';
 import { Container, PhotoCardBox } from './styles';
 import { Link } from 'react-router-dom';
 import { generateCategoryByCampType } from '@/libs/generateCategoryByCampType';
 
 function PhotoCardContent(props: Camp) {
-  const { thumbnail, skill, startDate, name, id, type } = props;
+  const { thumbnail, startDate, name, id, type } = props;
   return (
     <Link to={`/detail/${id}`}>
       <Container thumbnail= {thumbnail}>
           <PhotoCardBox>
             <span className='category'>{generateCategoryByCampType(type, props)}</span>
             <span className='title'>{name}</span>
-            <span className='due-date'>{startDate}부터</span>
+            <span className='due-date'>{dayjs(startDate).format('YYYY-MM-DD일 부터')}</span>
           </PhotoCardBox>
       </Container>
     </Link>
